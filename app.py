f61e893d191d36c74a476ca56ca8971db999aab4
am-muhammadumer <imumershaheen@gmail.com>
2025-01-15 01:20:53 +0500

    made simple app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Hardcoded API key
+API_KEY = "urem98u5m-jrlekjm09u540-9ojrlkejr0943uior"
+
+@app.route('/xyz', methods=['GET'])
+def get_api_key():
+    """
+    Endpoint to return the hardcoded API key.
+    """
+    return jsonify({"api_key": API_KEY})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
