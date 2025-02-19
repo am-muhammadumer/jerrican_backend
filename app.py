139f99165523d24d0221c9197dfd8e0cd565b036
am-muhammadumer <imumershaheen@gmail.com>
2025-02-19 23:33:21 +0500

    updated secret key

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # Hardcoded API key
-API_KEY = "urem98u5m-jrlekjm09u540-9ojrlkejr0943uior"
+API_KEY = "urem98u5m-jrlekjm09u540-9ojrlkejr0943uior0"
 
 @app.route('/xyz', methods=['GET'])
 def get_api_key():
